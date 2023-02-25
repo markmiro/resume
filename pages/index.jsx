f611ecea59e39bd078ec709f5369df9666470ee6
Mark Miro <contact@markmiro.com>
2023-02-24 19:35:19 -0800

    Changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,25 @@
 import { IBM_Plex_Mono, Inter, Space_Mono } from "next/font/google";
-import styled from "@emotion/styled";
-import tw from 'tailwind.macro'
+import _styled from "@emotion/styled";
 import React from "react";
-import { format } from "date-fns";
+import Head from "next/head";
 
 const ResumeContext = React.createContext();
 
 const inter = Inter({ subsets: ["latin"] });
-const spaceMono = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
-// const ibmMono = IBM_Plex_Mono({ subsets: ["latin"], weight: ["400", "700"] });
+// const spaceMono = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
+const ibmMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: ["500"],
+});
 
-const B = styled.div``;
-
-const Small = styled.span({  })`
-  font-size: 0.75em;
-  font-family: ${spaceMono.style.fontFamily};
-  font-weight: ${spaceMono.style.fontWeight};
+const Small = _styled.span`
+  font-size: 0.7em;
+  font-family: ${ibmMono.style.fontFamily};
+  font-weight: 500;
   text-transform: uppercase;
-  letter-spacing: 0.05em;
-  /* font-weight: 700; */
+  letter-spacing: 0.07em;
   opacity: 0.5;
 `;
-
-const BTitle = ({ children }) => <Small>{children}</Small>;
 
 const FirstLineBold = ({ children }) => {
   const [first, ...rest] = children.split("\n");
@@ -54,56 +51,72 @@ const Paragraphs = ({ children }) => {
   );
 };
 
-const BBody = ({ children }) => (
-  <div className="whitespace-nowrap">{children}</div>
-);
+const Name = () => {
+  const { name, nameRest, nameNote } = React.useContext(ResumeContext);
+  return (
+    <>
+      <div className="text-xs text-neutral-400 pb-1 border-b border-neutral-300 -mt-10">
+        <SplitNewLines>{nameNote}</SplitNewLines>
+      </div>
+      <div className="text-3xl font-bold">
+        {name}
+        <span className="border-r border-dashed border-neutral-300" />
+        <span className="opacity-20">{nameRest}</span>
+      </div>
+    </>
+  );
+};
+
+const HeaderHighlight = () => {
+  const { email, website, websiteText } = React.useContext(ResumeContext);
+  return (
+    <div className="relative">
+      <div className="absolute -top-4 -left-7 -right-7 -bottom-4 rounded-3xl bg-black bg-opacity-5 pointer-events-none" />
+      <Small>Portfolio</Small>
+      <div>
+        <a href={website}>
+          <b>{websiteText}</b>
+        </a>
+        <br />
+        <a href={`mailto:${email}`}>{email}</a>
+      </div>
+    </div>
+  );
+};
 
 const Heading = () => {
-  const { name, nameRest, email, title, website, websiteText, location } =
-    React.useContext(ResumeContext);
+  const { title, location } = React.useContext(ResumeContext);
   return (
     <div className="flex gap-2 justify-between items-end">
       <div>
-        <div className="text-3xl font-bold">
-          {name}
-          <span className="opacity-20">{nameRest}</span>
-        </div>
+        <Name />
         <div>{title}</div>
       </div>
-      <B>
-        <BTitle>Location</BTitle>
-        <BBody>
+      <div>
+        <Small>Location</Small>
+        <div>
           <FirstLineBold>{location}</FirstLineBold>
-        </BBody>
-      </B>
-      <B className="relative mr-4">
-        <div className="absolute -top-4 -left-7 -right-7 -bottom-4 rounded-3xl bg-black bg-opacity-5 pointer-events-none" />
-        <BTitle>Portfolio</BTitle>
-        <BBody>
-          <a href={website}>
-            <b>{websiteText}</b>
-          </a>
-          <br />
-          <a href={`mailto:${email}`}>{email}</a>
-        </BBody>
-      </B>
+        </div>
+      </div>
+      <div className="mr-4">
+        <HeaderHighlight />
+      </div>
     </div>
   );
 };
 
 const Skills = () => {
   const { skills } = React.useContext(ResumeContext);
-
   return (
     <div className="flex gap-2 justify-between">
       {/* <pre>{JSON.stringify(skills, null, 2)}</pre> */}
       {skills.map((skill) => (
-        <B>
-          <BTitle>{skill.title}</BTitle>
-          <BBody>
+        <div className="flex flex-col gap-1">
+          <b className="text-black">{skill.title}</b>
+          <Small>
             <SplitNewLines>{skill.desc}</SplitNewLines>
-          </BBody>
-        </B>
+          </Small>
+        </div>
       ))}
     </div>
   );
@@ -112,7 +125,7 @@ const Skills = () => {
 const Position = ({ org, title, from, to, skills, desc }) => (
   <>
     <div className="w-52">
-      <div>{org}</div>
+      <div className="font-bold">{org}</div>
       <Small>{title}</Small>
       <Small className="block">
         {from} – {to}
@@ -146,7 +159,7 @@ const Positions = () => {
 const Education = () => {
   const { education } = React.useContext(ResumeContext);
   return (
-    <div>
+    <div className="flex flex-col gap-1">
       <Small>Education</Small>
       <div>{education}</div>
     </div>
@@ -154,13 +167,11 @@ const Education = () => {
 };
 
 const Footer = () => {
-  const { ref } = React.useContext(ResumeContext);
-  const date = new Date();
-
+  const { ref, updated } = React.useContext(ResumeContext);
   return (
     <div className="flex justify-between">
       <Small>{ref}</Small>
-      <Small>Resume Updated: {format(date, "MMM dd, YYY")}</Small>
+      <Small>Resume Updated: {updated}</Small>
     </div>
   );
 };
@@ -168,7 +179,6 @@ const Footer = () => {
 export async function getStaticProps() {
   const yaml = require("js-yaml");
   const fs = require("fs");
-
   const doc = yaml.load(fs.readFileSync("./public/resume.yaml", "utf8"));
   return {
     props: {
@@ -180,19 +190,20 @@ export async function getStaticProps() {
 const LETTER_W = 8.5;
 const LETTER_H = 11;
 
-const Page = styled.div`
+const Page = _styled.div`
   width: 100vw;
   height: ${(100 * LETTER_H) / LETTER_W}vw;
   /* --- */
   font-family: ${inter.style.fontFamily};
-  /* background: red; */
-  /* border: 2px solid black; */
-  /* overflow-y: auto; */
 `;
 
 function Home(props) {
+  const { name, nameRest, updated } = props.doc;
   return (
     <>
+      <Head>
+        <title>{`${name}${nameRest} Resume - ${updated}`}</title>
+      </Head>
       <ResumeContext.Provider value={props.doc}>
         <Page className="flex flex-col justify-between p-16 shadow-2xl">
           <Heading />
@@ -204,7 +215,7 @@ function Home(props) {
           <hr />
           <Footer />
         </Page>
-        <pre className="print:hidden">{JSON.stringify(props.doc, null, 2)}</pre>
+        {/* <pre className="print:hidden">{JSON.stringify(props.doc, null, 2)}</pre> */}
       </ResumeContext.Provider>
     </>
   );
